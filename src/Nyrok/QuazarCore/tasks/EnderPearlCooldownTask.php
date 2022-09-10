fea6fecb48a7b7f75ec06ebfc4d693676b3a7c69
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-10 13:57:34 +0200

    update EnderPearlCooldownTask.php

--- src/Nyrok/QuazarCore/tasks/EnderPearlCooldownTask.php
+++ src/Nyrok/QuazarCore/tasks/EnderPearlCooldownTask.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace Nyrok\QuazarCore\tasks;
+
+use pocketmine\scheduler\Task;
+
+final class EnderPearlCooldownTask extends Task
+{
+    public function onRun(int $currentTick)
+    {
+        
+    }
+}
