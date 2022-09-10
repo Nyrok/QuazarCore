03e78dc007dfd9d7c929db222a70f6f703bb45e1
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-10 14:46:22 +0200

    update EnderPearlCooldownTask.php

--- src/Nyrok/QuazarCore/tasks/EnderPearlCooldownTask.php
+++ src/Nyrok/QuazarCore/tasks/EnderPearlCooldownTask.php
@@ -3,11 +3,30 @@
 namespace Nyrok\QuazarCore\tasks;
 
 use pocketmine\scheduler\Task;
+use pocketmine\Player;
+
+use Nyrok\QuazarCore\objects\Cooldown;
+use Nyrok\QuazarCore\Core;
 
 final class EnderPearlCooldownTask extends Task
 {
+    /**
+     * @param Cooldown $cooldown
+     * @param Player $player
+     */
+    public function __construct(private Cooldown $cooldown, private Player $player){}
+    
+    /**
+     * @param int $currentTick
+     */
     public function onRun(int $currentTick)
     {
-        
+        if($this->cooldown->has($this->player))
+        {
+            $player->getXpManager()->setXpLevel($this->cooldown->getCooldown());
+        }else{
+            $player->getXpManager()->setXpLevel(0);
+            Core::getInstance()->getScheduler()->cancelTask($this->getTaskId());
+        }
     }
 }
