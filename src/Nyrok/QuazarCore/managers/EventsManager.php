11f465d7d8b888e7c4dac97cbe8bd0686080bea1
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-11 10:18:34 +0200

    Création des fonctions

--- src/Nyrok/QuazarCore/managers/EventsManager.php
+++ src/Nyrok/QuazarCore/managers/EventsManager.php
@@ -1,2 +1,57 @@
 <?php
 
+namespace Nyrok\QuazarCore\managers;
+
+
+
+abstract class EventsManager
+{
+    /**
+     * @var Events[]
+     */
+    public static array $events = [];
+    
+    /**
+     * @return array
+     */
+    public static function getEvents(): array
+    {
+        return self::$events;
+    }
+    
+    /**
+     * @param string $name
+     * @return Event
+     */
+    public static function getEvent(string $name): Event
+    {
+        return self::$events[$name];
+    }
+    
+    /**
+     * @param Event $event
+     * @return void
+     */
+    public static function createEvent(Event $event): void
+    {
+        
+    }
+    
+    /**
+     * @param Event $event
+     * @return void
+     */
+    public static function startEvent(Event $event): void
+    {
+        
+    }
+    
+    /**
+     * @param Event $event
+     * @return void
+     */
+    public static function endEvent(Event $event): void
+    {
+        
+    }
+}
