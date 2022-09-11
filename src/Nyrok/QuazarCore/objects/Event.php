37453ec950cd9efb990194a7581dfb0ddcd70e0b
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-11 16:04:33 +0200

    Créations de fonctions

--- src/Nyrok/QuazarCore/objects/Event.php
+++ src/Nyrok/QuazarCore/objects/Event.php
@@ -2,9 +2,22 @@
 
 namespace Nyrok\QuazarCore\objects;
 
-
+use pocketmine\Player;
 
 final class Event
 {
+    /**
+     * @var $players[]
+     */
+    public static array $players = [];
     
+    /**
+     * @param string $name
+     * @param string $type
+     * @param Player $host
+     */
+    public function __construct(string $name, string $type, Player $host)
+    {
+        self::$players[$host];
+    }
 }
