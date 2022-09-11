da4cda0d8a6cf664355f8cdda715a72f7b76848f
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-11 11:55:45 +0200

    Modifications pour les ui

--- src/Nyrok/QuazarCore/commands/EventCommand.php
+++ src/Nyrok/QuazarCore/commands/EventCommand.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace Nyrok\QuazarCore\commands;
+
+use Nyrok\QuazarCore\managers\EventsManager;
+use Nyrok\QuazarCore\objects\Event;
+use pocketmine\Server;
+use Nyrok\QuazarCore\librairies\EasyUI\element\Button;
+use Nyrok\QuazarCore\librairies\EasyUI\variant\SimpleForm;
+use Nyrok\QuazarCore\managers\ArenasManager;
+use Nyrok\QuazarCore\providers\LanguageProvider;
+use Nyrok\QuazarCore\providers\PlayerProvider;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+final class EventCommand extends QuazarCommands
+{
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void
+    {
+        if (!$this->testPermissionSilent($sender)) return;
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(LanguageProvider::getLanguageMessage("messages.errors.not-a-player", null, true));
+            return;
+        }
+        $this->eventsForm($sender);
+    }
+    
+    public function eventsForm(Player $player): void
+    {
+        $form = new SimpleForm("§m§aEvents");
+        foreach (EventsManager::getEvents() as $event) {
+            $form->addButton(new Button($mode->getName(), null, ));
+        }
+        $player->sendForm($form);
+    }
+}
