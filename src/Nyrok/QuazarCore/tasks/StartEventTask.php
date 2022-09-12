7e5cfdd86c12610715d4d5c872c818d3db7f3f26
RemBog <87894259+RemBog@users.noreply.github.com>
2022-09-12 10:05:45 +0200

    Ajout du code...

--- src/Nyrok/QuazarCore/tasks/StartEventTask.php
+++ src/Nyrok/QuazarCore/tasks/StartEventTask.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace Nyrok\QuazarCore\tasks;
+
+use pocketmine\scheduler\Task;
+
+final class StartEventTask extends Task
+{
+    /**
+     * @param int $currentTick
+     */
+    public function onRun(int $currentTick)
+    {
+        foreach(EventsManager::getEvents() as $event)
+        {
+            if(!$event->getStart()) {
+                if($event->getStartIn() - time <= 0) {
+                    $event->setStart();
+                    EventsManager::startEvent($event);
+                }
+            }
+        }
+    }
+}
