48e49ccaf9042c134c7fef8fe1bf4b1faeedc5d0
Nyrok <47859726+Nyrok@users.noreply.github.com>
2022-09-24 08:18:01 +0200

    Update CosmeticsTrait.php

--- src/Nyrok/QuazarCore/traits/CosmeticsTrait.php
+++ src/Nyrok/QuazarCore/traits/CosmeticsTrait.php
@@ -4,6 +4,7 @@ namespace Nyrok\QuazarCore\traits;
 
 use GdImage;
 use Nyrok\QuazarCore\Core;
+use Nyrok\QuazarCore\librairies\EasyUI\EasyUI;
 use Nyrok\QuazarCore\librairies\EasyUI\variant\SimpleForm;
 use Nyrok\QuazarCore\managers\CosmeticsManager;
 use pocketmine\entity\Skin;
@@ -11,21 +12,22 @@ use pocketmine\Player;
 
 trait CosmeticsTrait
 {
-    private static function checkCosmetique(): void {
+    private static function checkCosmetique(): void
+    {
         $checkFileAvailable = [];
         $path = Core::getInstance()->getDataFolder();
         $allDirs = scandir($path);
         foreach ($allDirs as $foldersName) {
-            if(is_dir($path.$foldersName)){
-                array_push(CosmeticsManager::$cosmeticsTypes,$foldersName);
-                $allFiles = scandir($path.$foldersName);
+            if (is_dir($path . $foldersName) and $foldersName === "cosmetics") {
+                array_push(CosmeticsManager::$cosmeticsTypes, $foldersName);
+                $allFiles = scandir($path . $foldersName);
                 foreach ($allFiles as $allFilesName) {
-                    if(strpos($allFilesName, ".json")) {
+                    if (strpos($allFilesName, ".json")) {
                         array_push($checkFileAvailable, str_replace('.json', '', $allFilesName));
                     }
                 }
                 foreach ($checkFileAvailable as $value) {
-                    if(!in_array($value.".png", $allFiles)) {
+                    if (!in_array($value . ".png", $allFiles)) {
                         unset($checkFileAvailable[array_search($value, $checkFileAvailable)]);
                     }
                 }
@@ -36,45 +38,46 @@ trait CosmeticsTrait
         }
         unset(CosmeticsManager::$cosmeticsTypes[0]);
         unset(CosmeticsManager::$cosmeticsTypes[1]);
-        unset(CosmeticsManager::$cosmeticsTypes[array_search("saveskin",CosmeticsManager::$cosmeticsTypes)]);
+        unset(CosmeticsManager::$cosmeticsTypes[array_search("saveskin", CosmeticsManager::$cosmeticsTypes)]);
         unset(CosmeticsManager::$cosmeticsDetails["."]);
         unset(CosmeticsManager::$cosmeticsDetails[".."]);
         unset(CosmeticsManager::$cosmeticsDetails["saveskin"]);
         sort(CosmeticsManager::$cosmeticsTypes);
     }
 
-    private static function checkRequirement() {
+    private static function checkRequirement()
+    {
         $main = Core::getInstance();
-        if(!extension_loaded("gd")) {
+        if (!extension_loaded("gd")) {
             $main->getServer()->getLogger()->info("§6Uncomment gd2.dll (remove symbol ';' in ';extension=php_gd2.dll') in bin/php/php.ini to make the plugin working");
             $main->getServer()->getPluginManager()->disablePlugin($main);
             return;
         }
-        if(!class_exists(SimpleForm::class)) {
+        if (!class_exists(EasyUI::class)) {
             $main->getServer()->getLogger()->info("§6EasyUI class missing,pls use .phar from poggit!");
             $main->getServer()->getPluginManager()->disablePlugin($main);
             return;
         }
-        if(!file_exists($main->getDataFolder()."steve.json") || !file_exists($main->getDataFolder()."config.yml")) {
-            if(file_exists(str_replace("config.yml", "", $main->getResources()["config.yml"]))) {
-                self::recurse_copy(str_replace("config.yml","",$main->getResources()["config.yml"]),$main->getDataFolder());
-            }else {
+        if (!file_exists($main->getDataFolder() . "steve.json") || !file_exists($main->getDataFolder() . "config.yml")) {
+            if (file_exists(str_replace("config.yml", "", $main->getResources()["config.yml"]))) {
+                self::recurse_copy(str_replace("config.yml", "", $main->getResources()["config.yml"]), $main->getDataFolder());
+            } else {
                 $main->getServer()->getLogger()->info("§6Something wrong with the resources");
                 $main->getServer()->getPluginManager()->disablePlugin($main);
             }
         }
     }
 
-    private static function recurse_copy($src,$dst) {
+    private static function recurse_copy($src, $dst)
+    {
         $dir = opendir($src);
         @mkdir($dst);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' )) {
-                if ( is_dir($src . '/' . $file) ) {
-                    self::recurse_copy($src . '/' . $file,$dst . '/' . $file);
-                }
-                else {
-                    copy($src . '/' . $file,$dst . '/' . $file);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    self::recurse_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
                 }
             }
         }
