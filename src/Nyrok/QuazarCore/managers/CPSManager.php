a5ce5e1fcad6e8d9d436e33639e5d11a5a5b7953
RemBog <t.deventer16@gmail.com>
2022-12-28 15:00:18 +0100

    Fix CPS part. 2 (end)

--- src/Nyrok/QuazarCore/managers/CPSManager.php
+++ src/Nyrok/QuazarCore/managers/CPSManager.php
@@ -6,15 +6,8 @@ use Nyrok\QuazarCore\Core;
 use Nyrok\QuazarCore\librairies\CortexPE\DiscordWebhookAPI\Embed;
 use Nyrok\QuazarCore\librairies\CortexPE\DiscordWebhookAPI\Message;
 use Nyrok\QuazarCore\librairies\CortexPE\DiscordWebhookAPI\Webhook;
-use Nyrok\QuazarCore\listeners\DataPacketReceiveEvent;
 use Nyrok\QuazarCore\providers\PlayerProvider;
 use Nyrok\QuazarCore\tasks\CPSTask;
-use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
-use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
-use pocketmine\network\mcpe\protocol\PlayerActionPacket;
-use pocketmine\network\mcpe\protocol\types\inventory\UseItemOnEntityTransactionData;
-use pocketmine\network\mcpe\protocol\types\LevelSoundEvent;
-use pocketmine\network\mcpe\protocol\types\PlayerAction;
 use pocketmine\Player;
 
 abstract class CPSManager
@@ -57,11 +50,11 @@ abstract class CPSManager
      */
     public static function getCps(Player $player, float $deltaTime = 1.0, int $roundPrecision = 1): float
     {
-        if (!isset(self::$clicksData[$player->getName()]) || empty(self::$clicksData[$player->getName()])) {
+        if(!isset(self::$clicksData[mb_strtolower($player->getName())]) || empty(self::$clicksData[mb_strtolower($player->getName())])){
             return 0.0;
         }
         $ct = microtime(true);
-        return round(count(array_filter(self::$clicksData[$player->getName()], static function (float $t) use ($deltaTime, $ct) : bool {
+        return round(count(array_filter(self::$clicksData[mb_strtolower($player->getName())], static function(float $t) use ($deltaTime, $ct) : bool{
                 return ($ct - $t) <= $deltaTime;
             })) / $deltaTime, $roundPrecision);
     }
